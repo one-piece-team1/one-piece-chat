e770320f2cb917f4e038bf912e3b611d21a7e9bc
lib <dreak603@gmail.com>
2021-03-26 00:57:20 +0800

    feat: adding receiving chat status event  with kafka

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ormConfig } from './config/orm.config';
 import { ChatModule } from './chats/chat.module';
 import { ChatRoomModule } from './chatrooms/chat-room.module';
+import { ChatEventConsumerService } from './consumers/chat.consumer';
 
 @Module({
   imports: [TypeOrmModule.forRoot(ormConfig), ChatModule, ChatRoomModule],
+  providers: [ChatEventConsumerService],
 })
 export class AppModule {}
